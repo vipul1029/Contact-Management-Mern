2fefcaf586910d1fdcc5b9174385bf6d8d5cbdaf
Vipul Kumar <tovipul.kr@gmail.com>
2026-01-03 17:17:54 +0530

    Configure frontend for Vercel backend

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,3 +1,17 @@
+// import axios from "axios";
+
+// const API = axios.create({
+//   baseURL:
+//     process.env.REACT_APP_API_URL || "http://localhost:5000/api",
+//   headers: {
+//     "Content-Type": "application/json"
+//   }
+// });
+
+// export default API;
+
+
+
 import axios from "axios";
 
 const API = axios.create({
